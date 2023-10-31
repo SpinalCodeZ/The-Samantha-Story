bb2f7e75490cf7bb517e9d1aee86f7eca0926c9c
SpinalCodeZ <SpinalCodeZ@users.noreply.github.com>
2023-10-31 19:54:32 -0400

    I made some comments on the form1.cs file for my personal use to help orginize and decide on what to do.

--- The-Samantha-Story/Form1.cs
+++ The-Samantha-Story/Form1.cs
@@ -12,6 +12,9 @@ namespace The_Samantha_Story
 {
     public partial class theSamanthaStory : Form
     {
+
+        //add datatable to allow for save? do research and figure out best solution to holding game saves.
+
         public theSamanthaStory()
         {
             InitializeComponent();
@@ -19,32 +22,35 @@ namespace The_Samantha_Story
 
         private void yesButton_Click(object sender, EventArgs e)
         {
-
+            //link button to display outcome of text adventure
         }
 
         private void noButton_Click(object sender, EventArgs e)
         {
-
+            //link button to display outcome of text adventure
         }
 
         private void attackButton_Click(object sender, EventArgs e)
         {
-
+            //link button to display outcome of text adventure
         }
 
         private void defendButton_Click(object sender, EventArgs e)
         {
-
+            //link button to display outcome of text adventure
         }
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-
+            //link button to save the game 
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            //was going to use as a save holder but might change, still need to research the best option
         }
     }
 }
+
+
+//Learn how to display text and make it reactive to the button clicks of the user. Text box is for UI design ideas and not for game text display.
